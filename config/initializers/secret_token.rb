7e5230308607ce8cd769183f56c5c78b38eac000
destinyd <destinyd_war@163.com>
2013-04-01 18:00:29 +0800

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Yeshi::Application.config.secret_token = '03d75b03769e0cc5462eba43b0427d344d7fac4bf1e1b9811b18dbb95e6d9cbbd6c9962f4ac628d1561a5ac4ce87448a12b8eff8393ac3d5fbd8a7cd92f066af'
