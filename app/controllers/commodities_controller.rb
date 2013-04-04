7ce0c2a673ade20b79279d8b6128ccd5f3314608
destinyd <destinyd_war@163.com>
2013-04-04 10:35:47 +0800

    zh-CN and commodities show group show

--- app/controllers/commodities_controller.rb
+++ app/controllers/commodities_controller.rb
@@ -1,6 +1,6 @@
 class CommoditiesController < InheritedResources::Base
   respond_to :json
-  belongs_to :group
+  belongs_to :group, optional: true
   def collection
     @commodities ||= end_of_association_chain.opening.page params[:page]
   end
