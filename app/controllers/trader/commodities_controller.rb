253eb7cc2d09a82dc2c6488d2748903e16e25c9e
destinyd <destinyd_war@163.com>
2013-04-10 23:28:18 +0800

    json trader

--- app/controllers/trader/commodities_controller.rb
+++ app/controllers/trader/commodities_controller.rb
@@ -1,2 +1,7 @@
 class Trader::CommoditiesController < InheritedResources::Base
+  respond_to :json
+  belongs_to :group, optional: true
+  def collection
+    @commodities ||= end_of_association_chain.opening.recent.page params[:page]
+  end
 end
