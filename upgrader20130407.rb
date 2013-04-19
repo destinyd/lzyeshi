3c546c8d117e43e27cb8e31b00c32bad917b8cc0
destinyd <destinyd_war@163.com>
2013-04-19 15:47:57 +0800

    upgrader

--- upgrader20130407.rb
+++ upgrader20130407.rb
@@ -1,9 +1,13 @@
+@trader = Trader.asc(:created_at).first
+@user = User.asc(:created_at).first
 @groups = Group.scoped
 @groups.each do |group|
   group.update_attribute :commodities_count,group.commodities.count
+  group.update_attribute :trader_id,@trader.id
+  group.update_attribute :user_id,@user.id
 end
 
-@trader = Trader.scoped
+@traders = Trader.scoped
 @traders.each do |trader|
   trader.update_attribute :commodities_count,trader.commodities.count
 end
