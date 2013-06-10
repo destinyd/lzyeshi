f9dcb19c5853dd30b74e684a92ca1e95643ae36b
destinyd <destinyd_war@163.com>
2013-06-11 01:15:08 +0800

    session store for dev

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,6 +1,6 @@
 # Be sure to restart your server when you modify this file.
 
-Yeshi::Application.config.session_store :cookie_store, key: '_yeshi_session', domain: ".#{ENV['SUBDOMAIN'] + "." unless ENV['SUBDOMAIN'].blank?}liuzhouyeshi.com"
+Yeshi::Application.config.session_store :cookie_store, key: '_yeshi_session', domain: ".dev.liuzhouyeshi.com"
 
 # Use the database for sessions instead of the cookie-based default,
 # which shouldn't be used to store highly confidential information
