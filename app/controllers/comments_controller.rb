761f680afdc1994794efc6a34e078beed8adf567
destinyd <destinyd_war@163.com>
2013-06-16 13:11:22 +0800

    comments page

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -5,6 +5,16 @@ class CommentsController < InheritedResources::Base
   belongs_to :group, optional: true
   actions :index, :create
 
+  def index
+    index! do
+      if parent
+        add_crumb(I18n.t("controller.#{parent.class.name.downcase.pluralize}"), polymorphic_path(parent.class))
+        add_crumb(parent.name, polymorphic_path(parent))
+      end
+      add_crumb(I18n.t("controller.comments"))
+    end
+  end
+
   def create
     create! do |s,f|
       s.js{
