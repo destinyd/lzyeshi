f4d49bbef982a03dbcf290ff0ff4e58da0fdb81f
destinyd <destinyd_war@163.com>
2013-06-18 09:51:02 +0800

    remove diffent carrierwave images servers

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,6 +1,6 @@
-CarrierWave.configure do |config|
-  config.asset_host = proc do |file|
-    identifier = rand(0..6)
-    "http://images#{identifier}.ysdev.realityandapp.com"
-  end
-end
+#CarrierWave.configure do |config|
+  #config.asset_host = proc do |file|
+    #identifier = rand(0..6)
+    #"http://images#{identifier}.ysdev.realityandapp.com"
+  #end
+#end
