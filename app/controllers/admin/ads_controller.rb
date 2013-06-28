4c88e2b27a0662a09f5b3d0a4ad5b098558d03d4
destinyd <destinyd_war@163.com>
2013-06-28 14:04:27 +0800

    cancan for ads

--- app/controllers/admin/ads_controller.rb
+++ app/controllers/admin/ads_controller.rb
@@ -5,4 +5,9 @@ class Admin::AdsController < InheritedResources::Base
   def create
     create!{ads_path}
   end
+
+  protected
+  def collection
+    @ads ||= end_of_association_chain.accessible_by(current_ability).page params[:page]
+  end
 end
