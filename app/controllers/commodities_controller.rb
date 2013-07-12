10892d002231f6a58f680403a0040bbd57824f2e
destinyd <destinyd_war@163.com>
2013-07-12 18:43:53 +0800

    crummy in collection

--- app/controllers/commodities_controller.rb
+++ app/controllers/commodities_controller.rb
@@ -5,23 +5,6 @@ class CommoditiesController < InheritedResources::Base
   belongs_to :group, optional: true
   belongs_to :trader, optional: true
 
-
-  def index
-    index! do |f|
-      f.html{
-        if @trader
-          add_crumb(I18n.t("controller.traders"),trader_path(@trader))
-          add_crumb(@trader.to_s, trader_path(@trader))
-        end
-      add_crumb(I18n.t("controller.commodities"), commodities_path)
-      #add_crumb(:commodity) { [:group, :commodity] }
-      }
-      f.json{
-        render json: @commodities
-      }
-    end
-  end
-
   def show
     show! do 
       add_crumb(I18n.t("controller.commodities"), commodities_path)
@@ -31,6 +14,11 @@ class CommoditiesController < InheritedResources::Base
 
   protected
   def collection
+    if parent?
+      add_crumb(I18n.t("controller.traders"),trader_path(parent))
+      add_crumb(parent.to_s, trader_path(parent))
+    end
+    add_crumb(I18n.t("controller.commodities"), commodities_path)
     @commodities ||= end_of_association_chain.opening.recent.page params[:page]
   end
 end
